3102609dfae056219c069e9a140fe04264b892d8
samsonOdunola <odunolasamson@yahoo.com>
2023-07-03 14:51:47 +0100

    update readme

--- src/components/Contactlist.js
+++ src/components/Contactlist.js
@@ -4,15 +4,15 @@ import { useState, useEffect } from "react";
 
 export default function Contactlist() {
   const [loading, setLoading] = useState(true);
-  const [allUsers, setAllUsers] = useState();
-  const userRef = ref(db, "user");
+  const [allUsers, setAllUsers] = useState([]);
+  const userRef = ref(db);
 
   useEffect(() => {
     onValue(userRef, (snapshot) => {
-      console.log(snapshot.val());
+      console.log(snapshot.val().user);
       setAllUsers(snapshot.val());
       setLoading(false);
     });
   }, []);
-  return <>{loading ? "Loading" : <ul>{...allUsers}</ul>}</>;
+  return <>{loading ? "Loading" : <ul>working</ul>}</>;
 }
